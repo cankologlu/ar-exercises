3976563caeb268788166a930b82a0bda3656cc63
Can Kologlu <mckologlu@gmail.com>
2023-03-07 15:32:45 -0500

    add exercise 1 and 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,25 @@
 require_relative '../setup'
 
-puts "Exercise 1"
+
+puts "Exercise a"
 puts "----------"
 
 # Your code goes below here ...
+
+a = Store.new
+a.name = "Burnaby"
+a.annual_revenue = 300000
+a.mens_apparel = true
+a.womens_apparel = true
+a.save
+
+b = Store.new
+b.name = "Richmond"
+b.annual_revenue = 1260000
+b.mens_apparel = false
+b.womens_apparel = true
+b.save
+
+c = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "#{Store.count} stores created"
