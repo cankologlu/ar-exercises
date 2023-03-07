6201ef8fea30525283659815ee0e7c7a7c2bcd54
Can Kologlu <mckologlu@gmail.com>
2023-03-07 17:35:13 -0500

    validations in the models

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates: first_name, prescence: true
+  validates: last_name, prescence: true
+  validates: hourly_rate, numericality: { only_integer: true }
+  validates: hourly_rate, less_than: 200 
+  validates: hourly_rate, greater_than: 40
+  validates_associated: store
 end
